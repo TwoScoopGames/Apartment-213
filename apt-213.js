929c8085ac599a7bf5db3b3d18edc559beaec59a
Michael Revel <mjrevel@gmail.com>
2014-01-25 14:14:22 -0500

    more awesome comments 4 u bro

--- apt-213.js
+++ apt-213.js
@@ -2,9 +2,11 @@ var canvas = document.getElementById("game");
 
 var manifest = {
 	"images": {
-		"bg": "images/bg-mouse-1f4544x640.png",
-		"bgCat": "images/bg-cat-1f4544x640.png",
-		"mouse": "images/mouse-1f61x28.png"
+		"bg": 		"images/bg-mouse-1f4544x640.png",
+		"bgCat": 	"images/bg-cat-1f4544x640.png",
+		"mouse": 	"images/mouse-1f61x28.png",
+		"cat":		"images/cat-1f143x86.png"
+		//"owl":		"images/"	
 	},
 	"sounds": {
 	},
@@ -34,22 +36,28 @@ function(context) {
 });
 loading.start();
 
-var player;
+var playerMouse;
+var playerCat;
+var npcOwl;
+
 var furniture = [
 	new Splat.Entity(100, -200, 100, 100)
 ];
 
 function assetsLoaded() {
-	player = new Splat.AnimatedEntity(0, 0, 40, 8, apt213.images.get("mouse"), -15, -20);
 	
 	//stage1
-	scene1.camera = new Splat.EntityBoxCamera(player, 400, canvas.height, canvas.width/2, canvas.eight/2);
-	scene1.camera.y = -canvas.height + 100 + player.height;
+	playerMouse = new Splat.AnimatedEntity(0, 0, 40, 8, apt213.images.get("mouse"), -15, -20);
+	
+	scene1.camera = new Splat.EntityBoxCamera(playerMouse, 400, canvas.height, canvas.width/2, canvas.eight/2);
+	scene1.camera.y = -canvas.height + 100 + playerMouse.height;
 	
 	//stage2
-	scene2.camera = new Splat.EntityBoxCamera(player, 400, canvas.height, canvas.width/2, canvas.eight/2);
-	scene2.camera.y = -canvas.height + 100 + player.height;
+	playerCat = new Splat.AnimatedEntity(0, 0, 85, 15, apt213.images.get("cat"), -38, -70);
+	npcOwl = new Splat.AnimatedEntity(0, 0, 100, 15, apt213.images.get("owl"), -50, -50);
 	
+	scene2.camera = new Splat.EntityBoxCamera(playerMouse, 400, canvas.height, canvas.width/2, canvas.eight/2);
+	scene2.camera.y = -canvas.height + 100 + playerMouse.height;
 }
 
 function logMouseClick() {
@@ -68,28 +76,28 @@ scene1 = new Splat.Scene(canvas, function(elapsedMillis) {
 	for (var i in furniture) {
 		furniture[i].move(elapsedMillis);
 	}
-	player.move(elapsedMillis);
+	playerMouse.move(elapsedMillis);
 
 	for (var i in furniture) {
 		var f = furniture[i];
-		if (player.collides(f)) {
-			player.resolveCollisionWith(f);
+		if (playerMouse.collides(f)) {
+			playerMouse.resolveCollisionWith(f);
 		}
 	}
 
-	player.vx *= 0.5;
-	player.vy *= 0.75;
+	playerMouse.vx *= 0.5;
+	playerMouse.vy *= 0.75;
 	if (apt213.keyboard.isPressed("left")) {
-		player.vx = -0.7;
+		playerMouse.vx = -0.7;
 	}
 	if (apt213.keyboard.isPressed("right")) {
-		player.vx = 0.7;
+		playerMouse.vx = 0.7;
 	}
 	if (apt213.keyboard.isPressed("up")) {
-		player.vy = -0.2;
+		playerMouse.vy = -0.2;
 	}
 	if (apt213.keyboard.isPressed("down")) {
-		player.vy = 0.2;
+		playerMouse.vy = 0.2;
 	}
 },
 function(context) {
@@ -97,13 +105,13 @@ function(context) {
 		context.fillStyle = "#cccccc";
 		context.fillRect(0, 0, canvas.width, canvas.height);
 	});
-	context.drawImage(apt213.images.get("bg"), 0, -canvas.height + 100 + player.height);
+	context.drawImage(apt213.images.get("bg"), 0, -canvas.height + 100 + playerMouse.height);
 
 	for (var i in furniture) {
 		furniture[i].draw(context);
 	}
 
-	player.draw(context);
+	playerMouse.draw(context);
 });
 
 //**************** SCENE 2 ***********************
@@ -111,39 +119,39 @@ scene2 = new Splat.Scene(canvas, function(elapsedMillis) {
 	for (var i in furniture) {
 		furniture[i].move(elapsedMillis);
 	}
-	player.move(elapsedMillis);
+	playerCat.move(elapsedMillis);
 
 	for (var i in furniture) {
 		var f = furniture[i];
-		if (player.collides(f)) {
-			if (player.didOverlapHoriz(f) && player.wasAbove(f)) {
-				player.y = f.y - player.height - 0.01;
+		if (playerCat.collides(f)) {
+			if (playerCat.didOverlapHoriz(f) && playerCat.wasAbove(f)) {
+				playerCat.y = f.y - playerCat.height - 0.01;
 			}
-			if (player.didOverlapHoriz(f) && player.wasBelow(f)) {
-				player.y = f.y + f.height + 0.01;
+			if (playerCat.didOverlapHoriz(f) && playerCat.wasBelow(f)) {
+				playerCat.y = f.y + f.height + 0.01;
 			}
-			if (player.didOverlapVert(f) && player.wasLeft(f)) {
-				player.x = f.x - player.width - 0.01;
+			if (playerCat.didOverlapVert(f) && playerCat.wasLeft(f)) {
+				playerCat.x = f.x - playerCat.width - 0.01;
 			}
-			if (player.didOverlapVert(f) && player.wasRight(f)) {
-				player.x = f.x + f.width + 0.01;
+			if (playerCat.didOverlapVert(f) && playerCat.wasRight(f)) {
+				playerCat.x = f.x + f.width + 0.01;
 			}
 		}
 	}
 
-	player.vx *= 0.5;
-	player.vy *= 0.75;
+	playerCat.vx *= 0.5;
+	playerCat.vy *= 0.75;
 	if (apt213.keyboard.isPressed("left")) {
-		player.vx = -0.7;
+		playerCat.vx = -0.7;
 	}
 	if (apt213.keyboard.isPressed("right")) {
-		player.vx = 0.7;
+		playerCat.vx = 0.7;
 	}
 	if (apt213.keyboard.isPressed("up")) {
-		player.vy = -0.2;
+		playerCat.vy = -0.2;
 	}
 	if (apt213.keyboard.isPressed("down")) {
-		player.vy = 0.2;
+		playerCat.vy = 0.2;
 	}
 },
 function(context) {
@@ -151,11 +159,11 @@ function(context) {
 		context.fillStyle = "#cccccc";
 		context.fillRect(0, 0, canvas.width, canvas.height);
 	});
-	context.drawImage(apt213.images.get("bgCat"), 0, -canvas.height + 100 + player.height);
+	context.drawImage(apt213.images.get("bgCat"), 0, -canvas.height + 100 + playerCat.height);
 
 	for (var i in furniture) {
 		furniture[i].draw(context);
 	}
 
-	player.draw(context);
+	playerCat.draw(context);
 });
